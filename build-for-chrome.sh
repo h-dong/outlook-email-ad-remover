9ae4dbab6720e81c917b6d66e49d547a4b5c2ba9
Hao Dong <dong_hao@hotmail.co.uk>
2019-03-05 23:36:18 +0000

    Added bash script to create zips

--- build-for-chrome.sh
+++ build-for-chrome.sh
@@ -5,3 +5,5 @@ cp ./src/chrome/* ./dist/chrome/
 mkdir -p ./dist/chrome/icons/
 cp ./assets/icons/{16x16,32x32,48x48,128x128}.png ./dist/chrome/icons/
 cp ./src/contentScript.js ./dist/chrome/
+cd ./dist/chrome
+zip -r -X chrome_plugin.zip *
--- build-for-firefox.sh
+++ build-for-firefox.sh
@@ -5,3 +5,5 @@ cp ./src/firefox/* ./dist/firefox/
 mkdir -p ./dist/firefox/icons/
 cp ./assets/icons/{48x48,96x96}.png ./dist/firefox/icons/
 cp ./src/contentScript.js ./dist/firefox/
+cd ./dist/firefox
+zip -r -X firefox_plugin.zip *
